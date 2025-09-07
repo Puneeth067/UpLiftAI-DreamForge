fef629fca638e3bf0645605e4225ae89dde3979e
Puneeth067 <kumarpuneeth631@gmail.com>
2025-09-07 15:08:12 +0530

    build fixes for reload issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: true, // good for dev previews
+    port: 5173,
+  },
+  // Important for history fallback in production
+  build: {
+    outDir: "dist",
+  },
+});
